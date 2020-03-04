6a12ecdb09586dfa95c1318157c973034783d9dc
nb <anonimo2911.2012@gmail.com>
2020-03-04 17:36:23 -0500

    Repository UsuarioDao

---
+++ src/main/java/com/nb/springbootmicroserviceusuarios/models/dao/UsuarioDao.java
@@ -0,0 +1,15 @@
+package com.nb.springbootmicroserviceusuarios.models.dao;
+
+import com.nb.springbootmicroserviceusuarios.models.entity.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
+
+    public Usuario findByUsername(String username);
+
+    @Query("SELECT u FROM Usuario u WHERE u.username =?1")
+    public Usuario obtenerPorUsername(String username);
+
+
+}
