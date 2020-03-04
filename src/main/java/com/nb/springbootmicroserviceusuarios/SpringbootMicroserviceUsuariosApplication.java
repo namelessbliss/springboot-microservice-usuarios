5e696a8136194f5b2dc5e2d0c8f01d24b7819810
nb <anonimo2911.2012@gmail.com>
2020-03-04 16:30:51 -0500

    Proyecto inicial + properties

---
+++ src/main/java/com/nb/springbootmicroserviceusuarios/SpringbootMicroserviceUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceUsuariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nb/springbootmicroserviceusuarios/SpringbootMicroserviceUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
