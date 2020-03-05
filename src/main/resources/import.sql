eec2571edcf3a2219b3f8268be73bc58a199e573
nb <anonimo2911.2012@gmail.com>
2020-03-04 19:25:44 -0500

    añadiendo y asignando roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,9 @@
-INSERT INTO usuario(username, password,enabled,nombre,apellidos,email) VALUES ('adolfo','12345',1,'adolfo','p','nb@nb.com')
-INSERT INTO usuario(username, password,enabled,nombre,apellidos,email) VALUES ('andres','12345',1,'andres','p','nb@nb.com')
+INSERT INTO usuarios (username, password,enabled,nombre,apellidos,email) VALUES ('adolfo','12345',1,'adolfo','p','nb@nb.com')
+INSERT INTO usuarios (username, password,enabled,nombre,apellidos,email) VALUES ('andres','12345',1,'andres','p','nb2@nb.com')
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER')
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN')
+
+INSERT INTO usuarios_to_roles (usuario_id, rol_id) VALUES (1,1)
+INSERT INTO usuarios_to_roles (usuario_id, rol_id) VALUES (2,2)
+INSERT INTO usuarios_to_roles (usuario_id, rol_id) VALUES (2,1)
