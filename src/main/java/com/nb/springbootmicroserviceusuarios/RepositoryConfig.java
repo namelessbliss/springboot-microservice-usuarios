fc5aaae86caa3a1169494216d0dad186b9b52d34
nb <anonimo2911.2012@gmail.com>
2020-03-05 12:07:35 -0500

    Configuracion para exponer el id

---
+++ src/main/java/com/nb/springbootmicroserviceusuarios/RepositoryConfig.java
@@ -0,0 +1,22 @@
+package com.nb.springbootmicroserviceusuarios;
+
+import com.nb.springbootmicroserviceusuarios.models.entity.Rol;
+import com.nb.springbootmicroserviceusuarios.models.entity.Usuario;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+    /**
+     * Config para exponer el id en el json
+     *
+     * @param config
+     */
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Usuario.class, Rol.class);
+    }
+}
