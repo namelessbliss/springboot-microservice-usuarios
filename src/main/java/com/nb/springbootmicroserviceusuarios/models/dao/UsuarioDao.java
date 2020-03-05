df46d8c1342191423d39628612399a536b354c82
nb <anonimo2911.2012@gmail.com>
2020-03-04 20:28:58 -0500

    Personalizar end points de los metodos

--- src/main/java/com/nb/springbootmicroserviceusuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/nb/springbootmicroserviceusuarios/models/dao/UsuarioDao.java
@@ -3,12 +3,15 @@ package com.nb.springbootmicroserviceusuarios.models.dao;
 import com.nb.springbootmicroserviceusuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
-    public Usuario findByUsername(String username);
+    @RestResource(path = "buscar-username")
+    public Usuario findByUsername(@Param("nombre") String username);
 
     @Query("SELECT u FROM Usuario u WHERE u.username =?1")
     public Usuario obtenerPorUsername(String username);
